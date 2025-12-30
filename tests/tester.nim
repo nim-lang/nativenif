b26334fba13467699e020451798602eeb95613c7
araq <rumpf_a@web.de>
2025-12-30 07:15:36 +0100

    progress

--- tests/tester.nim
+++ tests/tester.nim
@@ -62,12 +62,14 @@ else:
   exec "nim c -r src/nifasm/nifasm tests/call_multi_result.nif"
   exec "./tests/call_multi_result"
   exec "nim c -r src/nifasm/nifasm tests/call_result_binding.nif"
-  execExpectFailure("nim c -r src/nifasm/nifasm tests/double_bind.nif", "Register RAX is already bound to variable 'x.0'")
-  execExpectFailure("nim c -r src/nifasm/nifasm tests/triple_bind.nif", "Register RAX is already bound to variable 'x.0'")
-  execExpectFailure("nim c -r src/nifasm/nifasm tests/quadruple_bind.nif", "Register RAX is already bound to variable 'x.0'")
-  execExpectFailure("nim c -r src/nifasm/nifasm tests/kill_use_after_kill.nif", "Expected variable or register as destination")
-  execExpectFailure("nim c -r src/nifasm/nifasm tests/missing_result_binding.nif", "Missing result binding: ret.0")
-  execExpectFailure("nim c -r src/nifasm/nifasm tests/stack_result_binding.nif", "Result 'ret.0' must be bound to a register")
-  execExpectFailure("nim c -r src/nifasm/nifasm tests/result_type_mismatch.nif", "Type mismatch:")
-  execExpectFailure("nim c -r src/nifasm/nifasm tests/call_missing_argument.nif", "Missing argument: arg.1")
-  execExpectFailure("nim c -r src/nifasm/nifasm tests/call_duplicate_result_binding.nif", "Duplicate result binding: ret.0")
+
+# Failing tests are not platform specific!
+execExpectFailure("nim c -r src/nifasm/nifasm tests/double_bind.nif", "Register RAX is already bound to variable 'x.0'")
+execExpectFailure("nim c -r src/nifasm/nifasm tests/triple_bind.nif", "Register RAX is already bound to variable 'x.0'")
+execExpectFailure("nim c -r src/nifasm/nifasm tests/quadruple_bind.nif", "Register RAX is already bound to variable 'x.0'")
+execExpectFailure("nim c -r src/nifasm/nifasm tests/kill_use_after_kill.nif", "Expected variable or register as destination")
+execExpectFailure("nim c -r src/nifasm/nifasm tests/missing_result_binding.nif", "Missing result binding: ret.0")
+execExpectFailure("nim c -r src/nifasm/nifasm tests/stack_result_binding.nif", "Result 'ret.0' must be bound to a register")
+execExpectFailure("nim c -r src/nifasm/nifasm tests/result_type_mismatch.nif", "Type mismatch:")
+execExpectFailure("nim c -r src/nifasm/nifasm tests/call_missing_argument.nif", "Missing argument: arg.1")
+execExpectFailure("nim c -r src/nifasm/nifasm tests/call_duplicate_result_binding.nif", "Duplicate result binding: ret.0")
